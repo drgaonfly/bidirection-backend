161ea5df7ba7372cffac5f417194bbd5b2b751c3
hfpp2012copy <hfpp2012@gmail.com>
2024-06-17 16:19:14 +0800

    fix: createAfterSalesOrder permissions

--- src/routes/afterSalesOrderRoutes.ts
+++ src/routes/afterSalesOrderRoutes.ts
@@ -14,18 +14,18 @@ const router: Router = express.Router();
 
 router
   .route('/')
-  .get(protect, allow([ROLES.Admin, ROLES.Customer, ROLES.CustomerService, ROLES.OrderPlacer, ROLES.Reviewer]), getAfterSalesOrders)  // Get list of after sales orders
+  .get(protect, allow([ROLES.Admin, ROLES.CustomerService, ROLES.OrderPlacer, ROLES.Reviewer]), getAfterSalesOrders)  // Get list of after sales orders
   .post(protect, allow(ROLES.Admin), createAfterSalesOrder)  // Add a new after sales order
   .delete(protect, allow([ROLES.Admin]), deleteMultipleAfterSalesOrders);
 
 router
   .route('/:id')
-  .get(protect, allow([ROLES.Admin]), updateAfterSalesOrder)  // Get details of a specific after sales order
-  .put(protect, allow(ROLES.Admin), updateAfterSalesOrder)  // Update an after sales order
+  .get(protect, allow([ROLES.Admin, ROLES.CustomerService, ROLES.OrderPlacer, ROLES.Reviewer]), updateAfterSalesOrder)  // Get details of a specific after sales order
+  .put(protect, allow([ROLES.Admin, ROLES.CustomerService, ROLES.OrderPlacer, ROLES.Reviewer]), updateAfterSalesOrder)  // Update an after sales order
   .delete(protect, allow(ROLES.Admin), deleteAfterSalesOrder);  // Delete a specific after sales order
 
 router
   .route('/:id/review')
-  .put(protect, allow(ROLES.Admin), reviewAfterSalesOrder);
+  .put(protect, allow([ROLES.Admin, ROLES.CustomerService, ROLES.OrderPlacer, ROLES.Reviewer]), reviewAfterSalesOrder);
 
 export default router;

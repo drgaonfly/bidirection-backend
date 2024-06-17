922909930c3e9fa5ab1d4d4707fde559de78cf51
hfpp2012copy <hfpp2012@gmail.com>
2024-06-17 16:16:10 +0800

    fix: AssignmentRecords permissions

--- src/routes/accountAssignmentRecordRoutes.ts
+++ src/routes/accountAssignmentRecordRoutes.ts
@@ -15,15 +15,15 @@ import { ROLES } from '../constants';
 const router = express.Router();
 
 // Define the routes for the Account Assignment Record operations
-router.post('/', protect, allow([ROLES.Customer, ROLES.Admin, ROLES.CustomerService, ROLES.OrderPlacer]), createAccountAssignmentRecord);
-router.get('/', protect, allow([ROLES.Customer, ROLES.Admin, ROLES.CustomerService, ROLES.OrderPlacer]), getAllAccountAssignmentRecords);
+router.post('/', protect, allow([ROLES.Admin, ROLES.CustomerService, ROLES.OrderPlacer]), createAccountAssignmentRecord);
+router.get('/', protect, allow([ROLES.Admin, ROLES.CustomerService, ROLES.OrderPlacer]), getAllAccountAssignmentRecords);
 router.get('/export', protect, allow([ROLES.Admin]), exportAccountAssignmentRecordsToExcel);
-router.get('/:id', protect, allow([ROLES.Customer, ROLES.Admin]), getAccountAssignmentRecordById);
-router.put('/:id', protect, allow([ROLES.Customer, ROLES.Admin]), updateAccountAssignmentRecord);
+router.get('/:id', protect, allow([ROLES.Admin]), getAccountAssignmentRecordById);
+router.put('/:id', protect, allow([ROLES.Admin]), updateAccountAssignmentRecord);
 router.delete('/:id', protect, allow([ROLES.Admin]), deleteAccountAssignmentRecord);
 router.delete('/', protect, allow([ROLES.Admin]), deleteMultipleAssignmentRecords);
 
 
-router.post('/upload', protect, allow([ROLES.Customer, ROLES.Admin]), uploadAccountAssignmentRecords);
+router.post('/upload', protect, allow([ROLES.Admin]), uploadAccountAssignmentRecords);
 
 export default router;

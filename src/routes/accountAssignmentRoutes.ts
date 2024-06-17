23caeb72208bfb7992e424d2b8a4ca2c8c65c841
hfpp2012copy <hfpp2012@gmail.com>
2024-06-17 17:11:17 +0800

    fix: permissions

--- src/routes/accountAssignmentRoutes.ts
+++ src/routes/accountAssignmentRoutes.ts
@@ -14,12 +14,12 @@ import { ROLES } from '../constants';
 const router = express.Router();
 
 // Define the routes for the Account Assignment operations
-router.post('/', protect, allow([ROLES.Customer, ROLES.Admin, ROLES.CustomerService, ROLES.OrderPlacer]), createAssignment);
-router.get('/', protect, allow([ROLES.Customer, ROLES.Admin]), getAllAssignments);
-router.get('/:id', protect, allow([ROLES.Customer, ROLES.Admin]), getAssignmentById);
-router.put('/:id', protect, allow([ROLES.Customer, ROLES.Admin]), updateAssignment);
+router.post('/', protect, allow([ROLES.Admin, ROLES.CustomerService, ROLES.OrderPlacer]), createAssignment);
+router.get('/', protect, allow([ROLES.Admin]), getAllAssignments);
+router.get('/:id', protect, allow([ROLES.Admin]), getAssignmentById);
+router.put('/:id', protect, allow([ROLES.Admin]), updateAssignment);
 router.delete('/:id', protect, allow([ROLES.Admin]), deleteAssignment);
 router.delete('/', protect, allow([ROLES.Admin]), deleteMultipleAssignments);
-router.post('/available', protect, allow([ROLES.Customer, ROLES.Admin]), findAvailableAccounts);
+router.post('/available', protect, allow([ROLES.OrderPlacer, ROLES.Admin, ROLES.CustomerService]), findAvailableAccounts);
 
 export default router;

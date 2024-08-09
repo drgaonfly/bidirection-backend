acc0f483ca5bd0ed726441ffcd3aabb5c6a1fca7
dragonfly <2237676323@qq.com>
2024-08-09 14:31:17 +0800

    fix: fix role name query , delete menu children and updated permissionGroup

--- src/utils/checkMenu.ts
+++ src/utils/checkMenu.ts
@@ -1,36 +1,29 @@
-
 import { IUser } from '../models/user';
-import { IMenu } from '../models/menu'
+import { IMenu } from '../models/menu';
 import { IRole } from '../models/role';
 
 const checkMenu = (menus: IMenu[], user: IUser): IMenu[] => {
-    return menus.filter((menu) => {
-        if (menu.children) {
-            menu.children = checkMenu(menu.children, user);
-        }
+  return menus.filter((menu) => {
+    // 不再检查和递归处理 children 字段
 
-        if (user.isAdmin) {
-            return true;
-        }
+    if (user.isAdmin) {
+      return true;
+    }
 
-        const roles = user.roles;
+    const roles = user.roles;
 
-        if (!roles) {
-            return false;
-        }
+    if (!roles) {
+      return false;
+    }
 
-        if (
-            roles.some(
-                (role: IRole) =>
-                    role.permissions &&
-                    role.permissions.some((permission) => permission.id === menu.permission)
-            )
-        ) {
-            return true;
-        } else {
-            return false;
-        }
-    });
+    return roles.some(
+      (role: IRole) =>
+        role.permissions &&
+        role.permissions.some(
+          (permission) => permission.id === menu.permission,
+        ),
+    );
+  });
 };
 
 export default checkMenu;

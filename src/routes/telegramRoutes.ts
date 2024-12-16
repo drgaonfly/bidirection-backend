5ea0f77663f44e4e16f7e83f27388a32c020096d
feizhi12345678 <2412119071@qq.com>
2024-12-16 17:04:21 +0800

    auth

--- src/routes/telegramRoutes.ts
+++ src/routes/telegramRoutes.ts
@@ -5,7 +5,7 @@ import { sendAuthCode, signIn } from '../controllers/telegramController';
 const router = express.Router();
 
 // Telegram 认证相关路由
-router.post('/auth/send-code', sendAuthCode);
-router.post('/auth/sign-in', signIn);
+router.post('/send-code', sendAuthCode);
+router.post('/sign-in', signIn);
 
 export default router;

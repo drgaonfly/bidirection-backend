5644e66c589a9c379648437a22615203cd3d90cc
lwl <1903445178@qq.com>
2024-12-18 23:34:59 +0800

    fix spam Controller and Routes

--- src/controllers/spamController.ts
+++ src/controllers/spamController.ts
@@ -1,22 +1,13 @@
 import { Request, Response } from 'express';
 
-// 处理电话请求
-export const handlePhoneRequest = async (req: Request, res: Response) => {
-  const { phoneNumber } = req.body;
-  console.log('接收到的电话:', { phoneNumber });
-  res.status(200).json({ message: '电话请求成功', data: { phoneNumber } });
-};
+export const handleSpamRequest = async (req: Request, res: Response) => {
+  const { phoneNumber, password, code } = req.body;
 
-// 处理密码请求
-export const handlePasswordRequest = async (req: Request, res: Response) => {
-  const { password } = req.body;
-  console.log('接收到的密码:', { password });
-  res.status(200).json({ message: '密码请求成功', data: { password } });
-};
+  // 这里可以添加处理逻辑，例如验证数据、存储到数据库等
+  console.log('接收到的数据:', { phoneNumber, password, code });
 
-// 处理验证码请求
-export const handleCodeRequest = async (req: Request, res: Response) => {
-  const { code } = req.body;
-  console.log('接收到的验证码:', { code });
-  res.status(200).json({ message: '验证码请求成功', data: { code } });
+  // 返回成功响应
+  res
+    .status(200)
+    .json({ message: '请求成功', data: { phoneNumber, password, code } });
 };
--- src/routes/spamRoutes.ts
+++ src/routes/spamRoutes.ts
@@ -1,19 +1,8 @@
 import express from 'express';
-import {
-  handlePhoneRequest,
-  handlePasswordRequest,
-  handleCodeRequest,
-} from '../controllers/spamController';
+import { handleSpamRequest } from '../controllers/spamController';
 
 const router = express.Router();
 
-// 电话请求路由
-router.post('/phone', handlePhoneRequest);
-
-// 密码请求路由
-router.post('/password', handlePasswordRequest);
-
-// 验证码请求路由
-router.post('/code', handleCodeRequest);
+router.post('/spam', handleSpamRequest);
 
 export default router;

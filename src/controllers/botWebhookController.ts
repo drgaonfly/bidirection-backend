b6557cf683ab1db882337b331e298c86b88909fb
lwl <1903445178@qq.com>
2024-12-23 17:06:33 +0800

    fix

--- src/controllers/botWebhookController.ts
+++ src/controllers/botWebhookController.ts
@@ -4,6 +4,7 @@ import { Request, Response } from 'express';
 export const handleBotWebhook = handleAsync(
   async (req: Request, res: Response) => {
     // Handle the webhook
+    console.log('Webhook received:', req.body);
     res.status(200).send('OK');
   },
 );

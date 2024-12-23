c68e7bd26928791578e3af1ee37a392098592e05
ganweiliang <ganweiliang@swqztech.com>
2024-12-23 17:40:22 +0800

    fix: webhook

--- src/controllers/botWebhookController.ts
+++ src/controllers/botWebhookController.ts
@@ -1,10 +1,24 @@
 import handleAsync from '../utils/handleAsync';
 import { Request, Response } from 'express';
+import { default as BotManager } from '../models/bot';
+import { setupBot } from '../bot/botSetup';
 
 export const handleBotWebhook = handleAsync(
-  async (req: Request, res: Response) => {
+  async (req: Request, res: Response): Promise<void> => {
     // Handle the webhook
     console.log('Webhook received:', req.body);
-    res.status(200).send('OK');
+
+    const botId = req.params.id;
+
+    const botManager = await BotManager.findOne({ isActive: true, _id: botId });
+
+    if (!botManager) {
+      res.status(404);
+      throw new Error('bot not found');
+    }
+
+    const bot = setupBot(botManager.token);
+
+    await bot.handleUpdate(req.body);
   },
 );

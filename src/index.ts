50559223112d93d9eb627c8255ecae2fc20608cc
dragonfly <2237676323@qq.com>
2025-01-03 13:54:47 +0800

    add instructions

--- src/index.ts
+++ src/index.ts
@@ -23,6 +23,7 @@ import topicRoutes from './routes/topicRoutes';
 import answerRoutes from './routes/answerRoutes';
 import proxyRoutes from './routes/proxyRoutes';
 import employeeRoutes from './routes/employeeRoutes';
+import instructionRoutes from './routes/instructionRoutes';
 
 import http from 'http';
 import { setupSocket } from './services/socket'; // 引入 socket 服务
@@ -60,6 +61,7 @@ app.use('/api/answers', answerRoutes);
 app.use('/api/topics', topicRoutes);
 app.use('/api/proxies', proxyRoutes);
 app.use('/api/employees', employeeRoutes);
+app.use('/api/instructions', instructionRoutes);
 
 app.use('/api/static', express.static(path.join(__dirname, 'uploads')));
 

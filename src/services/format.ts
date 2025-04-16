4f3a88f3d1f1e9f8eacb59ad3e1d43558723c477
efihfaifjw <2218344129@qq.com>
2025-04-16 11:59:38 +0800

    fix: update

--- src/services/format.ts
+++ src/services/format.ts
@@ -1,7 +1,9 @@
-export const formatUSDT = (amount: number): number => {
-  return Number(amount.toFixed(3));
+import BigNumber from 'bignumber.js';
+
+export const formatUSDT = (amount: number): string => {
+  return new BigNumber(amount).decimalPlaces(3).toString();
 };
 
-export const formatETH = (amount: number): number => {
-  return Number(amount.toFixed(5));
+export const formatETH = (amount: number): string => {
+  return new BigNumber(amount).decimalPlaces(6).toString();
 };

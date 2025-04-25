6acbd3a62ffe865ba830625260cbec8e3b541dc4
John Doe <john@example.com>
2025-04-25 18:55:46 +0800

    feat(socket): 添加用户和客户加入房间的功能
    在socket连接时，根据userId和customerId将用户和客户分别加入对应的房间，以便后续消息的定向发送

--- src/services/socket.ts
+++ src/services/socket.ts
@@ -11,6 +11,21 @@ export const setupSocket = async (server: http.Server): Promise<Server> => {
 
     console.log(`用户连接: ${socket.id}, token: ${token}`);
 
+    const userId = socket.handshake.query.userId as string;
+
+    console.log(`用户连接: ${socket.id}, userId: ${userId}`);
+    // 后台用户加入房间
+    if (userId) {
+      socket.join(`user-${userId}`);
+    }
+
+    const customerId = socket.handshake.query.customerId as string;
+    console.log(`客户连接: ${socket.id}, customerId: ${customerId}`);
+    // 前端客户加入房间
+    if (customerId) {
+      socket.join(`customer-${customerId}`);
+    }
+
     socket.on('disconnect', () => {
       console.log('客户端断开连接');
     });

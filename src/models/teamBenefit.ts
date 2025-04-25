8bf9942079c1d01648cfb7f2fe45a8ae317adfb6
efihfaifjw <2218344129@qq.com>
2025-04-25 14:45:21 +0800

    fix: 添加递归逻辑

--- src/models/teamBenefit.ts
+++ src/models/teamBenefit.ts
@@ -3,8 +3,10 @@ import { ICustomer } from './customer';
 
 export interface ITeamBenefit extends Document {
   customer: mongoose.Schema.Types.ObjectId | ICustomer;
+  parent: mongoose.Schema.Types.ObjectId | ICustomer; // 添加父级字段
   usdtIncome: number;
   ethIncome: number; // 以太坊实时收益
+  incomeRate: number; // 添加收益率字段
   toAddress: string; // 收款地址
   toNetwork: string; // 网络
   fromAddress: string; // 转出地址
@@ -20,7 +22,13 @@ const teamBenefitSchema = new mongoose.Schema(
       ref: 'Customer',
       required: true,
     },
+    parent: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Customer',
+      required: true,
+    },
     depth: { type: Number, required: true }, // 所在地址产生收益时的深度
+    incomeRate: { type: Number, required: true }, // 收益率
     ethIncome: { type: Number, default: 0 }, // 以太坊实时收益
     usdtIncome: { type: Number, required: true }, // USDT收益
     fromAddress: { type: String, required: true }, // 转出地址

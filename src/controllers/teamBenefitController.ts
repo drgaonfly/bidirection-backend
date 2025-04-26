317496f84e5804df98404fc80a0a6518216cc653
efihfaifjw <2218344129@qq.com>
2025-04-26 15:03:45 +0800

    fix: 填充parent

--- src/controllers/teamBenefitController.ts
+++ src/controllers/teamBenefitController.ts
@@ -46,6 +46,7 @@ export const getTeamBenefitList = handleAsync(
     const query = await buildQuery(req.query);
 
     const teamBenefit = await TeamBenefit.find(query)
+      .populate('parent')
       .populate('customer')
       .sort({ createdAt: -1 })
       .limit(+pageSize)

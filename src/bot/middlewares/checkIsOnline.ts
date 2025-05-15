4b28ea4bd560a1b67b9b206c7c89ae844782640d
John Doe <john@example.com>
2025-05-15 16:45:18 +0800

    fix(中间件): 在群组未设置开始时发送回复消息
    在 `checkIsOnline` 中间件中，当群组未设置开始时，除了记录调试信息外，现在还会向用户发送一条回复消息，提示群组未设置开始。这有助于用户更清楚地了解当前状态。

--- src/bot/middlewares/checkIsOnline.ts
+++ src/bot/middlewares/checkIsOnline.ts
@@ -9,6 +9,7 @@ export const checkIsOnline = async (
 ) => {
   if (!ctx.currentGroup.isOnline) {
     debug('该群组未设置开始');
+    ctx.reply('该群组未设置开始');
     // ctx.reply('请在群组中使用此命令');
     return;
   } else {

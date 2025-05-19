acea225e5366a3de85fc260def6238a6debaa6e4
John Doe <john@example.com>
2025-05-19 13:40:36 +0800

    feat: 添加支付模型以支持支付功能
    添加了 `payment.ts` 文件，定义了支付模型 `IPayment` 和 `paymentSchema`，用于存储支付相关信息，包括钱包、金额、状态、交易哈希、创建时间、过期时间、发送地址、接收地址和货币类型。该模型支持 USDT_ERC20 和 USDT_TRC20 两种货币类型，并默认使用 USDT_TRC20。

---
+++ src/models/payment.ts
@@ -0,0 +1,39 @@
+import mongoose, { Document, Schema } from 'mongoose';
+
+export interface IPayment extends Document {
+  wallet: Schema.Types.ObjectId;
+  amount: number;
+  status: 'pending' | 'paid' | 'expired';
+  txHash?: string;
+  createdAt: Date;
+  expiresAt: Date;
+  sendAddress: string;
+  receiveAddress?: string;
+  currency: 'USDT_ERC20' | 'USDT_TRC20';
+}
+
+const paymentSchema = new Schema<IPayment>(
+  {
+    wallet: { type: Schema.Types.ObjectId, ref: 'Wallet', required: true },
+    amount: { type: Number, required: true },
+    status: {
+      type: String,
+      enum: ['pending', 'paid', 'expired'],
+      default: 'pending',
+    },
+    txHash: String,
+    expiresAt: { type: Date, required: true },
+    sendAddress: { type: String, required: false },
+    currency: {
+      type: String,
+      enum: ['USDT_ERC20', 'USDT_TRC20'],
+      default: 'USDT_TRC20',
+    },
+    receiveAddress: { type: String, required: false },
+  },
+  { timestamps: true },
+);
+
+const Payment = mongoose.model<IPayment>('Payment', paymentSchema);
+
+export default Payment;

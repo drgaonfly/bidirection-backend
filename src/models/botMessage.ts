b99c6eb371d57d864e655eb435c049918fecb302
John Doe <john@example.com>
2025-05-26 10:30:37 +0800

    refactor(botMessage): update bot and group references in IBotMessage model
    - Modified the IBotMessage interface to allow bot and group fields to accept both ObjectId and their respective interfaces (IBot and IGroup).
    - This change enhances type safety and improves the model's flexibility for handling bot messages.

--- src/models/botMessage.ts
+++ src/models/botMessage.ts
@@ -1,14 +1,15 @@
 import mongoose, { Document } from 'mongoose';
 import { IBotUser } from './botUser';
-
+import { IBot } from './bot';
+import { IGroup } from './group';
 // 只存客户发给机器人的消息（toBot），不存机器人发给客户的消息（fromBot）
 export interface IBotMessage extends Document {
-  bot: mongoose.Schema.Types.ObjectId; // 关联的机器人
+  bot: mongoose.Schema.Types.ObjectId | IBot; // 关联的机器人
   botUser?: mongoose.Schema.Types.ObjectId | IBotUser; // 发送消息的 BotUser
   messageType: string; // 消息类型，如 text, image, command 等
   content: string; // 消息内容
   raw?: any; // 原始消息体，可选
-  group?: mongoose.Schema.Types.ObjectId; // 关联的群（如果是群消息）
+  group?: mongoose.Schema.Types.ObjectId | IGroup; // 关联的群（如果是群消息）
   // direction 字段移除，始终为 toBot
 }
 

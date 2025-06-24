be97b6538250af2c3fa5db9012fb7a67b6394ff2
dragonfly <2237676323@qq.com>
2025-06-24 10:50:35 +0800

    feat(groupMessage): enhance sendGroupMessages function with improved error handling, logging, and support for local signed URLs for images

--- src/tasks/cron/groupMessager.ts
+++ src/tasks/cron/groupMessager.ts
@@ -3,6 +3,8 @@ import { IBot } from '../../models/bot';
 import { IGroup } from '../../models/group';
 import { formatBeijingDate } from '../../utils/formatBeijingDate';
 import { setupBot } from '../../bot/botSetup';
+import { generateLocalSignedUrl } from '../../utils/generateSignedUrl';
+import { InputFile } from 'grammy';
 
 /**
  * 群发消息任务
@@ -53,6 +55,9 @@ export async function sendGroupMessages() {
         const hoursSinceLastSent =
           (currentTime.getTime() - lastSentTime.getTime()) / (1000 * 60 * 60);
 
+        console.log('hoursSinceLastSent', hoursSinceLastSent);
+        console.log('intervalHours', intervalHours);
+
         if (hoursSinceLastSent < intervalHours) {
           console.log(
             `[sendGroupMessages] 消息 ${message._id} 距离上次发送不足 ${intervalHours} 小时，跳过`,
@@ -67,33 +72,37 @@ export async function sendGroupMessages() {
 
         // 向每个群组发送消息
         for (const group of groups) {
-          try {
-            // 发送消息前先验证群组
-            await telegramBot.api.getChat(group.id);
+          if (!group) {
+            console.log(`[sendGroupMessage] 群组不存在: ${group}`);
+            continue;
+          }
+          // 如果成功，才发送消息
+          if (message.image) {
+            const processed_image = await generateLocalSignedUrl(message.image);
 
-            // 如果成功，才发送消息
-            if (message.image) {
-              // 发送带图片的消息
+            if (processed_image.includes('localhost')) {
               await telegramBot.api.sendPhoto(
                 group.id,
-                `${process.env.BACKEND_URL}/api/static/${message.image}`,
+                new InputFile(`tmp/${message.image}`),
                 {
                   caption: message.content,
                   parse_mode: 'HTML',
                 },
               );
             } else {
-              // 发送纯文本消息
-              await telegramBot.api.sendMessage(group.id, message.content, {
+              await telegramBot.api.sendPhoto(group.id, processed_image, {
+                caption: message.content,
                 parse_mode: 'HTML',
               });
             }
-            sentCount++;
-            console.log(`[sendGroupMessages] 已向群组 ${group.id} 发送消息`);
-          } catch (error) {
-            console.log(`群组 ${group.id} 不存在或机器人未加入，标记为无效`);
-            // 可以更新数据库，标记此群组为无效
+          } else {
+            // 发送纯文本消息
+            await telegramBot.api.sendMessage(group.id, message.content, {
+              parse_mode: 'HTML',
+            });
           }
+          sentCount++;
+          console.log(`[sendGroupMessages] 已向群组 ${group.id} 发送消息`);
         }
 
         // 只有当消息至少发送到一个群组时才更新发送时间

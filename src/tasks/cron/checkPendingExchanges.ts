966c146510103144896e1862800a0a6589934b7c
John Doe <john@example.com>
2025-06-28 15:58:32 +0800

    feat(checkPendingExchanges): add logging for matched transfer's from_address during exchange processing

--- src/tasks/cron/checkPendingExchanges.ts
+++ src/tasks/cron/checkPendingExchanges.ts
@@ -77,6 +77,11 @@ export async function checkPendingExchanges() {
         continue;
       }
 
+      console.log(
+        'matchedTransfer.from_address:',
+        matchedTransfer.from_address,
+      );
+
       // if (!exchange.isTransferIntoOther) {
       exchange.to_address = matchedTransfer.from_address;
       await exchange.save();
